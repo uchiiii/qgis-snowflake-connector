9247573935b1ed788001ac5e50fcc3ae988326a2
uchiiii <uchikun17@gmail.com>
2025-11-27 16:29:29 +0900

    Remove order randomly

--- providers/sf_feature_iterator.py
+++ providers/sf_feature_iterator.py
@@ -228,7 +228,8 @@ class SFFeatureIterator(QgsAbstractFeatureIterator):
 
             order_limit_clause = ""
             if self._provider._is_limited_unordered:
-                order_limit_clause = f" ORDER BY RANDOM() LIMIT {limit_size_for_type(self._provider._geo_column_type, self._provider._connection_name)}"
+                # No need to order randomly when limiting rows, which is time-consuming
+                order_limit_clause = f" LIMIT {limit_size_for_type(self._provider._geo_column_type, self._provider._connection_name)}"
 
             self.final_query = (
                 "select * from ("
